fb85af687861e6f955bbaea587ef212b89ef05e5
accakks <aakanksha.jain8@gmail.com>
2018-06-11 11:40:24 +0530

    GSoC code added

--- Plan  for Outreachy/outreachy.py
+++ Plan  for Outreachy/outreachy.py
@@ -11,10 +11,23 @@ def CommonOrgs():
                 CommonOrgsl.append(s)
     print(CommonOrgsl)
 
-##def SummerGSoCCommon():
- 
 
-##def WinterGsocCommon():
+def SummerGSoCCommon():
+    SummerGSoCCommonl = []
+    for s in summerOrgs:
+        for g in GSoC_orgs:
+            if s == g:
+                SummerGSoCCommonl.append(s)
+    print(SummerGSoCCommonl)
+    
+    
+def WinterGsocCommon():
+    WinterGsocCommonl = []
+    for w in winterOrgs:
+        for g in GSoC_orgs:
+            if w == g:
+                WinterGsocCommonl.append(w)
+    print(WinterGsocCommonl)
 
 
 def CheckOrg():
@@ -65,8 +78,21 @@ for w in range(i, len(organisation)-1):
     winterOrgs.append((organisation[w].string).lower())
 
 
+##GSoC orgs parser
 
+f = open('gsoc.html')
+html_content_2 = f
 
+soup_2 = BeautifulSoup(html_content_2, "html.parser")
+
+f.close()
+
+GSoC_organisation = soup_2.findAll("div", {"class" :"organization-card__name font-black-54"})
+
+GSoC_orgs = []
+
+for i in range(len(GSoC_organisation)):
+    GSoC_orgs.append((GSoC_organisation[i].string).lower())
 
 #Driver part
 
@@ -94,6 +120,4 @@ elif n == 5:
     SummerOrgsI()
 elif n == 6:
     WinterOrgsI()
-
-
 
